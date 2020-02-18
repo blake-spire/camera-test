5013b762d8c786e88befc379d6a9289bc181a2cb
blake <blake.netzeband@spiredigital.com>
2020-02-18 09:58:12 -0700

    Updates

---
+++ precache-manifest.63a0912abbaced44e398516028efd448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8bb3dfc0d4d21ecb33d",
+    "url": "/camera-test/static/css/main.2a46f3c9.chunk.css"
+  },
+  {
+    "revision": "d8bb3dfc0d4d21ecb33d",
+    "url": "/camera-test/static/js/main.c5642a50.chunk.js"
+  },
+  {
+    "revision": "46337b82324860182abd",
+    "url": "/camera-test/static/js/runtime~main.df1b26b5.js"
+  },
+  {
+    "revision": "0763691a1307ecb9b5e9",
+    "url": "/camera-test/static/js/2.a84edf2b.chunk.js"
+  },
+  {
+    "revision": "c74ab23c61eaa067ae2010d1893eab14",
+    "url": "/camera-test/index.html"
+  }
+];
